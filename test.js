01bdedb79c3b9b0e840d1fee929c97849a70468f
Denis Maslennikov <nof1000@mail.ru>
2017-02-10 18:11:39 +0300

    A bit refactoring of the testing

--- test.js
+++ test.js
@@ -6,16 +6,16 @@ const test = require('ava');
 const token = process.env.TOKEN;
 
 /** Tests */
-test.before('`Token` should be is defined!', (t) => {
-    t.truthy(token, '`token` is defined.');
+test.before('`TOKEN` must be is defined!', (t) => {
+    t.truthy(token);
 });
 
 test('Should be created a new instance', (t) => {
     const api = teleapi(token);
-    t.truthy(api, 'is created!');
+    t.truthy(api);
 });
 
-test('Should be created a custom methods', (t) => {
+test('Should be created a new instance with custom methods', (t) => {
     const api = teleapi(token, {
         version: '1.0-custom',
         methods: [
@@ -23,10 +23,10 @@ test('Should be created a custom methods', (t) => {
         ],
     });
 
-    t.truthy(api.getMe, 'is created!');
+    t.truthy(api.getMe);
 });
 
-test('Should be an error', (t) => {
+test('Should throw an error', (t) => {
     const api = teleapi(token);
     return api.sendMessage().then((data) => {
         t.fail();
@@ -35,7 +35,7 @@ test('Should be an error', (t) => {
     });
 });
 
-test('Should get information about bot', (t) => {
+test('Should get description of bot', (t) => {
     const api = teleapi(token);
     return api.getMe().then((data) => {
         if (data.id && data.first_name && data.username) {
