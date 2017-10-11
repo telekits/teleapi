08ff75e72bc1d9fae5bb3b78f3bcf68d06076871
Denis Maslennikov <mrdenniska@gmail.com>
2017-10-11 18:38:11 +0300

    [TEST]: Tests has been rewritten to Mocha 🐇

--- test.js
+++ test.js
@@ -1,59 +1,83 @@
+/**
+ * A contain the testing of teleapi.
+ *
+ * @author Denis Maslennikov <mrdenniska@gmail.com> (nofach.com)
+ * @license MIT
+ */
+
 /** Dependencies */
 const { randomBytes } = require('crypto');
 const teleapi = require('./index.js');
-const test = require('ava');
+const assert = require('assert');
 
-/** Token for testing */
-const token = process.env.TOKEN;
+/** Preparing */
+const TOKEN = process.env.TOKEN;
+assert.ok(TOKEN, 'The environment must be contain a `TOKEN` variable before you run test.');
 
-/** Tests */
-test.before('`TOKEN` must be is defined in ENV!', (t) => {
-    t.truthy(token);
-});
-
-test('Checking presence of the default methods and api version', (t) => {
-    t.truthy(teleapi.methods);
-    t.truthy(teleapi.version);
-});
-
-test('Just trying to create new instance', (t) => {
-    const api = teleapi(token);
-
-    t.truthy(api);
-});
-
-test('Trying to create new instance with custom api', (t) => {
-    const api = teleapi(token, {
-        version: '1.0-custom',
-        methods: ['getMe'],
+/** Testing */
+describe('Exports', () => {
+    it('should be as a Function', () => {
+        assert.equal(Object.getPrototypeOf(teleapi), Function.prototype);
     });
 
-    t.truthy(api.getMe);
+    it('should be contained a `version` variable', () => {
+        assert.ok(teleapi.version);
+    });
+
+    it('should be contained a `methods` variable', () => {
+        assert.ok(teleapi.methods);
+    });
 });
 
-test('Request to sendMessage without params', (t) => {
-    const api = teleapi(token);
+describe('Instance', () => {
+    it('should return new instance', () => {
+        const api = teleapi(TOKEN);
 
-    api.sendMessage()
-        .then(() => t.fail())
-        .catch(() => t.pass());
+        assert.ok(api);
+    });
+
+    it('should return new instance with custom api', () => {
+        const methodName = 'getMe';
+        const version = '1.0-custom';
+        const api = teleapi(TOKEN, {
+            version: '1.0-custom',
+            methods: [methodName],
+        });
+
+        assert.ok(api);
+
+        assert.equal(api.version, version);
+        assert.ok(api.getMe);
+    });
 });
 
-test('Request to random method without params', (t) => {
-    const method = `random-${randomBytes(16).toString('hex')}`;
-    const api = teleapi(token);
+describe('API', () => {
+    it('should request to sendMessage without params and get throw an error', (done) => {
+        const api = teleapi(TOKEN);
 
-    api.method(method)
-        .then(() => t.fail())
-        .catch(() => t.pass());
+        api.sendMessage()
+            .then(() => done(new Error("Request wasn't to pass")))
+            .catch(() => done());
+    });
+
+    it('should request to random method without params and get throw an error', (done) => {
+        const method = `random-${randomBytes(16).toString('hex')}`;
+        const api = teleapi(TOKEN);
+
+        api.method(method)
+            .then(() => done(new Error("Request wasn't to pass")))
+            .catch(() => done());
+    });
+
+    it('should be received an information about the bot', (done) => {
+        const api = teleapi(TOKEN);
+
+        api.getMe().
+            then((response) => {
+                assert.ok(response.id);
+                assert.ok(response.is_bot);
+                assert.ok(response.username);
+                done();
+            });
+    });
 });
-
-test('Get the description of the bot', (t) => {
-    const api = teleapi(token);
-
-    api.getMe()
-        .then((data) => (data.id && data.username) ? t.pass() : t.fail())
-        .catch((error) => t.fail());
-});
-
-
